c7cd73f236355f78a2ecfe4df41dc7956518ce20
Ramil Valiev <ramulkazan89@gmail.com>
2020-06-19 13:09:22 +0400

    lesson07

---
+++ ru/valiev/lesson07/task1/Drinks.java
@@ -0,0 +1,22 @@
+package ru.valiev.lesson07.task1;
+
+public enum Drinks {
+    COLA(1),
+    SPRITE(3),
+    JUICE(4),
+    FANTA(2),
+    MIRINDA(3);
+
+    private int price;
+    Drinks(int price) {
+        this.price = price;
+
+    }
+
+    public int getPrise() {
+        return price;
+    }
+
+
+}
+
---
+++ ru/valiev/lesson07/task1/VendingMachine.java
@@ -0,0 +1,78 @@
+package ru.valiev.lesson07.task1;
+
+import java.util.Scanner;
+
+import static ru.valiev.lesson07.task1.Drinks.*;
+
+public class VendingMachine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("MENU:");
+        System.out.println("1. " + Drinks.COLA + " -    " + COLA.getPrise() + "$");
+        System.out.println("2. " + Drinks.SPRITE + " -  " + SPRITE.getPrise() + "$");
+        System.out.println("3. " + Drinks.JUICE + " -   " + JUICE.getPrise() + "$");
+        System.out.println("4. " + Drinks.FANTA + " -   " + FANTA.getPrise() + "$");
+        System.out.println("5. " + Drinks.MIRINDA + " - " + MIRINDA.getPrise() + "$");
+
+
+        System.out.println("Make a doposit to select a drink!");
+        int deposit = scanner.nextInt();
+        while (true) {
+            if (deposit < 1) {
+                System.out.println("Make a deposit!!!");
+                deposit = scanner.nextInt();
+            } else {
+                System.out.println("You input " + deposit + "$");
+                break;
+            }
+        }
+
+        System.out.println("Please, choise a drink");
+
+        while (true) {
+            int select = scanner.nextInt();
+            if (select > 5 || select < 1) {
+                System.out.println("Number is not correct. Please try again!");
+
+            }else if (select == 1 & deposit >= COLA.getPrise()) {
+                System.out.println("Please, take your: " + COLA);
+                deposit = deposit - COLA.getPrise();
+                System.out.println("Deposit =  " + deposit + "$");
+                break;
+            } else  if (select == 2 & deposit >= SPRITE.getPrise()) {
+                System.out.println("Please, take your: " + SPRITE);
+                deposit = deposit - SPRITE.getPrise();
+                System.out.println("Deposit =  " + deposit + "$");
+                break;
+            } else  if (select == 3 & deposit >= JUICE.getPrise()) {
+                System.out.println("Please, take your: " + JUICE);
+                deposit = deposit - JUICE.getPrise();
+                System.out.println("Deposit =  " + deposit + "$");
+                break;
+            } else if (select == 4 & deposit >= FANTA.getPrise()) {
+                System.out.println("Please, take your: " + FANTA);
+                deposit = deposit - FANTA.getPrise();
+                System.out.println("Deposit =  " + deposit + "$");
+                break;
+            } else if (select == 5 & deposit >= MIRINDA.getPrise()) {
+                System.out.println("Please, take your: " + MIRINDA);
+                deposit = deposit - MIRINDA.getPrise();
+                System.out.println("Deposit =  " + deposit + "$");
+                break;
+            } else {
+                System.out.println("Insufficient funds. Please, choice other drink!");
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
