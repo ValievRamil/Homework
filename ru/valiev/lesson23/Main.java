c91661881c2d53b203daa9a1883d745a6a494d0a
Ramil Valiev <ramulkazan89@gmail.com>
2020-07-29 18:58:57 +0400

    lesson 23

---
+++ ru/valiev/lesson23/Main.java
@@ -0,0 +1,29 @@
+package ru.valiev.lesson23;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerBasket customerBasket = new CustomerBasket();
+        customerBasket.addProduct("Каша", 2);
+        customerBasket.addProduct("Водка", 15);
+        customerBasket.addProduct("Мясо", 7);
+        System.out.println(customerBasket.toString());
+        System.out.println();
+
+        customerBasket.addProduct("Каша", 5);
+        System.out.println(customerBasket.toString());
+
+        customerBasket.removeProduct("Водка");
+        System.out.println(customerBasket.toString());
+
+        customerBasket.updateProductQuantity("Колбаса", 5);
+
+        System.out.println(customerBasket.getProductQuantity("Водка"));
+
+        System.out.println(customerBasket.getProductQuantity("Каша"));
+        System.out.println(customerBasket.getProducts());
+        customerBasket.clear();
+        System.out.println(customerBasket.toString());
+    }
+}
