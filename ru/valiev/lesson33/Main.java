c33d9f9bc038a8a9d05c4f43f5c24b3ab8205f7b
Ramil Valiev <ramulkazan89@gmail.com>
2020-08-28 18:05:52 +0400

    lesson 33

---
+++ ru/valiev/lesson33/Main.java
@@ -0,0 +1,10 @@
+package ru.valiev.lesson33;
+
+import static ru.valiev.lesson33.maximumOccurring.maxOccur;
+
+public class Main {
+    public static void main(String[] args) {
+        String ip = "fsfsfsRPTPQJRLWQJHFKSJFHKS";
+        maxOccur(ip);
+    }
+}
